2a43c39507716e9a355d7f27d90ab2045c4ead74
GACHyt <146768965+GACHyt@users.noreply.github.com>
2024-06-25 14:33:16 -0300

    UI done

---
+++ Assets/gamemanagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanagerScript : MonoBehaviour
+{
+    public GameObject[] productos;
+    public int dinero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/productoScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class productoScript : MonoBehaviour
+{
+    public int precio;
+    void Start()
+    {
+        precio = Random.Range(1, 100);
+    }
+
+    void Update()
+    {
+        
+    }
+}

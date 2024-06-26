100fcf09ffe72a5bd97111ce424dc7f4e102905c
Matias Ariel Szpektor <matirulosz@gmail.com>
2024-06-26 16:22:45 -0300

    objetos creados e instanciado
    creo e instanceo los objetos, ademas cambio los text y empiezo las interacciones para mas adelante

--- Assets/gamemanagerScript.cs
+++ Assets/gamemanagerScript.cs
@@ -8,24 +8,32 @@ public class gamemanagerScript : MonoBehaviour
 {
     public Transform spawnPoint1;
     public Transform spawnPoint2;
-    public GameObject[] productos;
-    public int dinero;
+
+    //pase el array de productos para productoScript
+
+    public int dinero=100;
 
     public GameObject objeto1;
     public GameObject objeto2;
 
-    public Text uno;
-    public Text dos;
+    public Text presu;
+    public Text precioUno;
+    public Text precioDos;
 
     private int sumaProductos;
 
     void Start()
     {
 
-        objeto1 = Instantiate(productos[0], spawnPoint1.position, Quaternion.identity);
-        objeto2 = Instantiate(productos[0], spawnPoint1.position, Quaternion.identity);
+        objeto1 = Instantiate(gameObject, spawnPoint1.position, Quaternion.identity);// corregir bien el tema  de como crear/activar el objeto
+        objeto2 = Instantiate(gameObject, spawnPoint1.position, Quaternion.identity);
 
-        int precio1=objeto1.GetComponent<>();
+        int precio1= objeto1.GetComponent<productoScript>().precio;
+        int precio2 = objeto1.GetComponent<productoScript>().precio;
+
+        presu.text = $"Presupuesto: ${dinero}";
+        precioUno.text = $"${precio1}";
+        precioDos.text = $"${precio2}";
     }
 
     void Update()

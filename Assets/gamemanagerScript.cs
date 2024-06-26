9a27bcdccd7a36467c2d5c2498aa64a141dfeff0
Matias Ariel Szpektor <163904501+matiszpek@users.noreply.github.com>
2024-06-25 21:12:53 -0300

    inicio de programacion y (se buggearon cosas)

--- Assets/gamemanagerScript.cs
+++ Assets/gamemanagerScript.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class gamemanagerScript : MonoBehaviour
 {
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
     public GameObject[] productos;
     public int dinero;
 
+    public GameObject objeto1;
+    public GameObject objeto2;
+
+    public Text uno;
+    public Text dos;
+
+    private int sumaProductos;
+
     void Start()
     {
-        
+
+        objeto1 = Instantiate(productos[0], spawnPoint1.position, Quaternion.identity);
+        objeto2 = Instantiate(productos[0], spawnPoint1.position, Quaternion.identity);
+
+        int precio1=objeto1.GetComponent<>();
     }
 
     void Update()

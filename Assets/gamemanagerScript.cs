30fcb7a76d9f8cdc897878f4231da07364333ce5
GACHyt <146768965+GACHyt@users.noreply.github.com>
2024-06-27 10:12:30 -0300

    Ya no se crashea y genera bien los objetos

--- Assets/gamemanagerScript.cs
+++ Assets/gamemanagerScript.cs
@@ -13,8 +13,13 @@ public class gamemanagerScript : MonoBehaviour
 
     public int dinero=100;
 
-    public GameObject objeto1;
-    public GameObject objeto2;
+    public GameObject[] productosGenerator1;
+    public GameObject[] productosGenerator2;
+
+    GameObject obj1, obj2;
+
+    GameObject objeto1;
+    GameObject objeto2;
 
     public Text presu;
     public Text precioUno;
@@ -24,12 +29,16 @@ public class gamemanagerScript : MonoBehaviour
 
     void Start()
     {
-
-        objeto1 = Instantiate(gameObject, spawnPoint1.position, Quaternion.identity);// corregir bien el tema  de como crear/activar el objeto
-        objeto2 = Instantiate(gameObject, spawnPoint1.position, Quaternion.identity);
+        //productosGenerator2[Random.Range(0, productosGenerator2.Length)].SetActive(true);
+        obj1 = productosGenerator1[Random.Range(0, productosGenerator1.Length)];
+        obj1.SetActive(true);
+        obj2 = productosGenerator2[Random.Range(0, productosGenerator2.Length)];
+        obj2.SetActive(true);
+        objeto1 = Instantiate(obj1, spawnPoint1.position, Quaternion.identity);// corregir bien el tema  de como crear/activar el objeto
+        objeto2 = Instantiate(obj2, spawnPoint2.position, Quaternion.identity);
 
         int precio1= objeto1.GetComponent<productoScript>().precio;
-        int precio2 = objeto1.GetComponent<productoScript>().precio;
+        int precio2 = objeto2.GetComponent<productoScript>().precio;
 
         presu.text = $"Presupuesto: ${dinero}";
         precioUno.text = $"${precio1}";
--- Assets/productoScript.cs
+++ Assets/productoScript.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class productoScript : MonoBehaviour
 {
     public int precio;
-    public GameObject[] productosGenerator;
+    
 
     void Start()
     {
-        productosGenerator[Random.Range(0, productosGenerator.Length)].SetActive(true);
+        
         precio = Random.Range(1, 100);
     }
 
